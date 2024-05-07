f78c60839b2d1dd0e8d28b47841c2e86a591be35
BL4STORK <144135621+BL4STORK@users.noreply.github.com>
2024-05-07 11:24:21 +0200

    commit

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+//EXO-1
+#include <stdio.h>
+
+
+int main() {
+    int var1 = 1;
+    int var2 = 2;
+    int *Var1 = &var1;
+    int *Var2 = &var2;
+
+    var1 = *Var2;
+
+    printf("%d\n", var1);
+
+    return 0;
+}
+
+
+
+
+//EXO-2
+
 #include <stdio.h>
 #include "stdlib.h"
 
@@ -35,3 +57,102 @@ int main() {
 
     return 0;
 }
+
+//EXO-3
+
+#include <stdio.h>
+#include "stdlib.h"
+
+
+typedef struct element element;
+struct element{
+    int val;
+    struct element *nxt;
+};
+
+typedef element* liste_element;
+
+liste_element ajouterEnFin(liste_element liste, int valeur){
+    element* nouvelElement = malloc(sizeof(element));
+    nouvelElement->val = valeur;
+    nouvelElement->nxt = NULL;
+
+    if(liste == NULL){
+        return nouvelElement;
+    }
+    else{
+        element* temp=liste;
+        while(temp->nxt != NULL){
+            temp = temp->nxt;
+        }
+        temp->nxt = nouvelElement;
+        return liste;
+    }
+}
+
+void afficherListe(liste_element liste){
+    element *tmp = liste;
+    while(tmp != NULL){
+        printf("%d ", tmp->val);
+        tmp = tmp->nxt;
+    }
+}
+void supp_element(struct element* e){
+    if(e->nxt){
+        supp_element(e->nxt);
+    }
+    free(e);
+}
+
+int main() {
+    int taille_liste;
+    struct element* valeur_specifique;
+    printf("Veuillez saisir la taille de votre liste\n");
+    scanf("%d", &taille_liste);
+    liste_element ma_liste = NULL;
+    for (int i = 0; i < taille_liste; ++i) {
+        ma_liste = ajouterEnFin(ma_liste,i);
+    }
+    afficherListe(ma_liste);
+    supp_element(valeur_specifique);
+
+    return 0;
+}
+
+//EXO-4
+
+#include <stdio.h>
+#include "stdlib.h"
+
+struct Voiture{
+    char marque[50];
+    char modele[50];
+    int annee;
+}Voiture;
+
+void create_voiture ( int *nb_voitures){
+    printf("saisir le nombre de voiture :\n");
+    scanf("%d", nb_voitures);
+
+    for (int i = 0; i < *nb_voitures; ++i) {
+        printf("Veuillez saisir une marque: \n");
+        scanf("%s", Voiture.marque);
+        printf("Veuillez saisir un modele: \n");
+        scanf("%s", Voiture.modele);
+        printf("Veuillez saisir de quelle annee est le vehicule: \n");
+        scanf("%d", &Voiture.annee);
+    }
+
+
+}
+
+int main() {
+    int nb_voitures = 0;
+    create_voiture(&nb_voitures);
+    for (int i = 0; i < nb_voitures; ++i) {
+        printf("Voici les donnees de : %s, %s, %d \n",Voiture.marque, Voiture.modele, Voiture.annee);
+    }
+    return 0;
+}
+
+
